71b89a44ea6499a16710f983b7a1423008af4f26
Zhastilek Zhalelov <zzhalelov@gmail.com>
2025-01-14 23:56:34 +0500

    git init

---
+++ src/main/java/kz/zzhalelov/messengerspringbootpractice/MessengerSpringBootPracticeApplication.java
@@ -0,0 +1,13 @@
+package kz.zzhalelov.messengerspringbootpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerSpringBootPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessengerSpringBootPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/zzhalelov/messengerspringbootpractice/MessengerSpringBootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zzhalelov.messengerspringbootpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerSpringBootPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
